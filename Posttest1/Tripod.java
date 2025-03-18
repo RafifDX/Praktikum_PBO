f514b9983c50d7f52a88a9479e85b268377716b8
RafifDX <161927310+RafifDX@users.noreply.github.com>
2025-03-18 22:20:24 +0800

    Add files via upload

---
+++ Posttest1/Tripod.java
@@ -0,0 +1,36 @@
+public class Tripod {
+    private int id;
+    private String merk;
+    private double harga;
+
+    public Tripod(int id, String merk, double harga) {
+        this.id = id;
+        this.merk = merk;
+        this.harga = harga;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | Merk: " + merk + " | Harga: Rp" + String.format("%,.2f", harga);
+    }
+}
